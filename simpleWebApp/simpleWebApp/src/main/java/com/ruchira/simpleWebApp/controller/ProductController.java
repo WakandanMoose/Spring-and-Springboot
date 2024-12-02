4c077cfdea9a7ddf97ff3434aecf536e10bf3cd9
Ruchira De Silva <ruchiradesilva6@gmail.com>
2024-12-02 12:26:42 +0530

    Adding get and post mappings and methods

--- simpleWebApp/simpleWebApp/src/main/java/com/ruchira/simpleWebApp/controller/ProductController.java
+++ simpleWebApp/simpleWebApp/src/main/java/com/ruchira/simpleWebApp/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.ruchira.simpleWebApp.controller;
 import com.ruchira.simpleWebApp.model.Product;
 import com.ruchira.simpleWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +13,19 @@ public class ProductController {
     @Autowired
     ProductService service;
 
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProduct() {
         return service.getProduct();
+    }
+
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId) {
+        return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct(@RequestBody Product prod) {
+        System.out.println(prod);
+        service.addProduct(prod);
     }
 }
--- simpleWebApp/simpleWebApp/src/main/java/com/ruchira/simpleWebApp/service/ProductService.java
+++ simpleWebApp/simpleWebApp/src/main/java/com/ruchira/simpleWebApp/service/ProductService.java
@@ -3,18 +3,28 @@ package com.ruchira.simpleWebApp.service;
 import com.ruchira.simpleWebApp.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<>(Arrays.asList(
             new Product(101, "Iphone", 50000),
             new Product(102, "Cannon Camera", 70000),
-            new Product(103, "Shura Mic", 10000));
+            new Product(103, "Shura Mic", 10000)));
 
     public List<Product> getProduct() {
         return products;
+    }
+
+    public Product getProductById(int prodId) {
+        return products.stream()
+                .filter(p -> p.getProdId() == prodId).findFirst().get();
+    }
+
+    public void addProduct(Product prod) {
+        products.add(prod);
     }
 }
